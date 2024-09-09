d30562828b68f8851713934a4e40a170ac0cb66a
Hiten M <hiten.mahalwar@ucalgary.ca>
2024-09-09 01:41:58 -0600

    Working parallized identity model and face detection model to detect and label faces in live video

--- app/recognize.py
+++ app/recognize.py
@@ -1,47 +1,92 @@
 import cv2 as cv
 from transformers import pipeline
 import mediapipe as mp
+from PIL import Image
+import multiprocessing 
+from collections import defaultdict
 
 RES = (520*2, 293*2)
-WEB_CAM_ID = 1 # Device id of the video input device
+CLOSEUP_RES = (512,512)
+CLOSEUP_MARGIN = 100
+WEB_CAM_ID = 1 # Device id of the video input deviced
+GPU_ID = -1 # -1 for CPU, 0 for GPU
 
-# vid = cv.VideoCapture("Videos/group.mp4")
-vid = cv.VideoCapture(WEB_CAM_ID) 
-cTime, pTime = 0, 0
+def identity_worker(ids_of_interest, face_id_to_identity, face_id_to_closeup):
+    pipe = pipeline("image-classification", model="cledoux42/Ethnicity_Test_v003", device=GPU_ID)
 
-mpDraw = mp.solutions.drawing_utils
-mpFaceDetection = mp.solutions.face_detection.FaceDetection()
+    while True:
+        for id in ids_of_interest:
+            closeup = face_id_to_closeup.get(id, None)
+            if closeup is not None:
+                img = Image.fromarray(closeup)
+                # img.show()
+                res = pipe(img)
+                face_id_to_identity[id] = res[0]['label']
 
-while True:
-    cTime = cv.getTickCount()
-    fps = cv.getTickFrequency() / (cTime - pTime)
-    pTime = cTime
+def paint_face_box(framePainted, bbox, id, identity):
+    cv.rectangle(framePainted, bbox, (255, 0, 255), 2)
+    cv.putText(framePainted, f'ID: {id}, Identity: {identity}', (bbox[0], bbox[1]-20), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 1)
+    return framePainted
 
-    isTrue, frame = vid.read()
-    if not isTrue:
-        break
 
-    frame = cv.resize(frame, RES)
-    frameRGB = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-    framePainted = frame.copy()
+def main():
+    # vid = cv.VideoCapture("Videos/students.mp4")
+    vid = cv.VideoCapture(WEB_CAM_ID) 
+    cTime, pTime = 0, 0
 
-    faces = mpFaceDetection.process(frameRGB)
+    mpDraw = mp.solutions.drawing_utils
+    mpFaceDetection = mp.solutions.face_detection.FaceDetection()
 
-    if faces.detections:
-        for id, detection in enumerate(faces.detections):
-            bboxC = detection.location_data.relative_bounding_box
-            ih, iw, ic = framePainted.shape
-            bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih)
+    manager = multiprocessing.Manager()
+    face_id_to_identity = manager.dict()
+    face_id_to_closeup = manager.dict()
+    ids_of_interest = manager.list()
 
-            cv.rectangle(framePainted, bbox, (255, 0, 255), 2)
+    worker = multiprocessing.Process(target=identity_worker, args=(ids_of_interest,face_id_to_identity,face_id_to_closeup))
+    worker.start()
 
-            closeupBox = [bbox[0]-50, bbox[1]-50, bbox[2]+100, bbox[3]+100]
-            if bbox[0]-50 > 0 and bbox[1]-50 > 0:
-                closeup = frame[closeupBox[1]:closeupBox[1]+closeupBox[3], closeupBox[0]:closeupBox[0]+closeupBox[2]]
-                cv.imshow(f'Closeup_{id}', closeup)
+    while True:
+        cTime = cv.getTickCount()
+        fps = cv.getTickFrequency() / (cTime - pTime)
+        pTime = cTime
+
+        isTrue, frame = vid.read()
+        if not isTrue:
+            break
+
+        frame = cv.resize(frame, RES)
+        frameRGB = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        framePainted = frame.copy()
+
+        faces = mpFaceDetection.process(frameRGB)
+
+        newIds = []
+        if faces.detections:
+            for id, detection in enumerate(faces.detections):
+                bboxC = detection.location_data.relative_bounding_box
+                ih, iw, ic = framePainted.shape
+                bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih)
+
+
+                framePainted = paint_face_box(framePainted, bbox, id, face_id_to_identity.get(id, None))
+
+                closeupBox = [bbox[0]-CLOSEUP_MARGIN, bbox[1]-CLOSEUP_MARGIN, bbox[2]+CLOSEUP_MARGIN*2, bbox[3]+CLOSEUP_MARGIN*2]
+                if bbox[0]-CLOSEUP_MARGIN > 0 and bbox[1]-CLOSEUP_MARGIN > 0:
+                    closeup = frameRGB[closeupBox[1]:closeupBox[1]+closeupBox[3], closeupBox[0]:closeupBox[0]+closeupBox[2]]
+                    face_id_to_closeup[id] = cv.resize(closeup, CLOSEUP_RES)
+                    # cv.imshow(f'Closeup_{id}', face_id_to_closeup[id])
+                newIds.append(id)
+        ids_of_interest[:] = newIds
+
+        cv.putText(framePainted, f'FPS: {int(fps)}', (10, 50), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 1)
+        cv.imshow('Video', framePainted)
+
+        if cv.waitKey(20) & 0xFF==ord('d'):
+            break
     
-    cv.putText(framePainted, f'FPS: {int(fps)}', (10, 50), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 1)
-    cv.imshow('Video', framePainted)
+    worker.terminate()
+    worker.join()
+    return
 
-    if cv.waitKey(20) & 0xFF==ord('d'):
-        break
+if __name__ == "__main__":
+    main()
